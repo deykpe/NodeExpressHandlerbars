b05ecc69ca8f359ca521c5d41ff40a559a28c85a
David Pang <dpang2@uci.edu>
2019-12-11 17:03:23 -0800

    databaseupdate

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const port = 8080;
 var app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
+var exphbs  = require('express-handlebars');
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars"); 
+
+app.listen(port, function(){
+    console.log('port 8080 is working')
+})
+
