1d4b176656c3335a90bc22f8a53ae5f54d9ea4ea
David Pang <dpang2@uci.edu>
2019-12-15 12:12:26 -0800

    requiredorm

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ var exphbs  = require('express-handlebars');
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars"); 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
 app.listen(port, function(){
     console.log('port 8080 is working')
-})
+});
+
+
 
