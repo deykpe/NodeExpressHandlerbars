995d9963daee5da4c7264378960013a4e5b912c7
David Pang <dpang2@uci.edu>
2019-12-15 12:22:34 -0800

    added router,post,put

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,27 @@ var router = express.Router()
 router.get('/', function (req , res){
     res.redirect('/burgers')
 })
+
+router.get("/burgers", function(req, res) {
+ // express callback response by calling burger.selectAllBurger
+    burger.all(function(burgerData) {
+// wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+      res.render("index", { burger_data: burgerData });
+    });
+  });
+  router.post("/burgers/create", function(req, res) {
+    // express callback response by calling burger.selectAllBurger
+       burger.create(req.body.burger_name, function(burgerData) {
+   // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+         res.redirect("/");
+       });
+     });
+    router.put("/burgers/:id", function(req, res) {
+        // express callback response by calling burger.selectAllBurger
+           burger.update(req.params.id, function(burgerData) {
+       // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+             res.sendStatus(200)
+           });
+         });
+
+        module.exports = router;
